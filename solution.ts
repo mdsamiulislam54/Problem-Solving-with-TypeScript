8ded18d1696af94b83a99d5dcb94e4e17af12b68
mdsamiulislam54 <samiulm5332@gmail.com>
2025-11-17 10:55:34 +0600

    solved this problem Create a Person class with name and age properties. Add a method getDetails that returns a string with the person's name and age.

--- solution.ts
+++ solution.ts
@@ -1,21 +1,34 @@
-type valueType = string | number | boolean
-const formatValue = (value: valueType): valueType => {
-    if (typeof value === "string") {
-        return value.toUpperCase();
-    } else if (typeof value === 'number') {
-        return value * 10;
-    } else {
-        return value ? false : true
+// type valueType = string | number | boolean
+// const formatValue = (value: valueType): valueType => {
+//     if (typeof value === "string") {
+//         return value.toUpperCase();
+//     } else if (typeof value === 'number') {
+//         return value * 10;
+//     } else {
+//         return value ? false : true
+//     }
+
+// }
+
+// const getLength = <T>(value: string | T[]): number => {
+//     if (Array.isArray(value)) {
+//         return value.length
+//     } else {
+//         return value.length
+//     }
+// }
+
+
+class Person {
+    name: string;
+    age: number
+
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age
+    }
+    getDetails(){
+        return `'Name: ${this.name}, Age: ${this.age}'`
     }
 
 }
-
-
-const getLength = <T>(value: string | T[]): number => {
-    if (Array.isArray(value)) {
-        return value.length
-    } else {
-        return value.length
-    }
-}
-
