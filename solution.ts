b1236ed72b243fcfcc2ced08bdd3eaf81e2db97e
mdsamiulislam54 <samiulm5332@gmail.com>
2025-11-17 10:45:00 +0600

    Create a function getLength that accepts a value which may be a string or an array, and returns the length of the value. solved this problem

--- solution.ts
+++ solution.ts
@@ -1,11 +1,21 @@
 type valueType = string | number | boolean
-const formatValue=(value:valueType )=>{
-    if(typeof value === "string"){
+const formatValue = (value: valueType): valueType => {
+    if (typeof value === "string") {
         return value.toUpperCase();
-    }else if(typeof value === 'number'){
-        return value*10;
-    }else{
+    } else if (typeof value === 'number') {
+        return value * 10;
+    } else {
         return value ? false : true
     }
 
-} 
+}
+
+
+const getLength = <T>(value: string | T[]): number => {
+    if (Array.isArray(value)) {
+        return value.length
+    } else {
+        return value.length
+    }
+}
+
